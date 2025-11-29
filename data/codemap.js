5953f5e4ef99d7b62d6b6a5cbf680b584e243054
Gaël Maignan <gael.maignan@eivp-paris.fr>
2025-11-29 20:47:00 +0200

    Add files via upload

---
+++ data/codemap.js
@@ -0,0 +1,53 @@
+// codeMap.js
+
+const codeMap = {
+  "1":   "Patrimoine bâti",
+    "11":  "Culte",
+      "111": "Orthodoxe (églises, ekklisakia)",
+
+    "12":  "Historique",
+      "121": "Antique (agora antique)",
+
+    "13":  "Remarquable",
+      "131": "Maisons remarquables",
+
+    "14":  "Ruines et bâtiments abandonnés",
+      "141": "Ruines",
+      "142": "Bâtiments abandonnés / désaffectés",
+
+    "15":  "Friches",
+      "151": "Friche urbaine",
+      "152": "Friche végétalisée",
+      "153": "Friche industrielle",
+
+  "2":   "Mobilier urbain",
+    "21":  "Mobilier fonctionnel",
+
+    "22":  "Mobilier technique",
+
+  "3":   "Transport et mobilité",
+    "31":  "Motorisée",
+
+    "32":  "Douce",
+
+  "4":   "Art, représentation & symbolique urbaine",
+    "41":  "Art officiel",
+      "411": "Installations artistiques",
+
+    "42":  "Graffitis & inscriptions urbaines",
+      "421": "Symboles anarchistes",
+      "422": "Motifs récurrents",
+      "423": "Graffitis divers",
+
+  "5":   "Expressions et signes socio-politiques",
+    "51":  "Manifestations",
+      "511": "Rassemblements",
+      "512": "Processions / marches",
+
+    "52":  "Mouvements et revendications",
+      "521": "Anarchisme",
+      "522": "Autres mouvements",
+      "523": "Affiches / stickers militants",
+
+  "6":   "Commerces et services urbains"
+};
